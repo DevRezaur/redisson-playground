ea5a96d0fd3547ac5c27698596a6324f451ea43d
Rezaur Rahman <os-rezaur.rahman@rakuten.com>
2023-07-20 12:02:20 +0600

    Updated KeyValueTest.java

--- src/test/java/com/devrezaur/main/KeyValueTest.java
+++ src/test/java/com/devrezaur/main/KeyValueTest.java
@@ -6,17 +6,72 @@ import org.redisson.client.codec.StringCodec;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 public class KeyValueTest extends BaseTestClass {
 
     @Test
     public void keyValueAccessTest() {
         RBucketReactive<String> bucket = this.client.getBucket("user:1:name", StringCodec.INSTANCE);
         Mono<Void> set = bucket.set("Fahim Faysal");
-        Mono<String> get = bucket.get().doOnNext(System.out::println);
+        Mono<String> get = bucket.get();
 
+        // Testing if the data is saved to redis properly
         StepVerifier
                 .create(set.then(get))
                 .expectNext("Fahim Faysal")
+                .verifyComplete();
+    }
+
+    @Test
+    public void keyValueExpiryTest() {
+        RBucketReactive<String> bucket = this.client.getBucket("user:1:name", StringCodec.INSTANCE);
+        Mono<Void> set = bucket.set("Fahim Faysal", 3, TimeUnit.SECONDS);
+        Mono<String> get = bucket.get();
+
+        // Testing if the key is saved to redis
+        StepVerifier
+                .create(set.then(get))
+                .expectNextCount(1)
+                .verifyComplete();
+
+        // Testing if the key exist in redis after 3 seconds
+        sleep(3000);
+
+        StepVerifier
+                .create(get)
+                .expectNextCount(0)
+                .verifyComplete();
+    }
+
+    @Test
+    public void keyValueExtendExpiryTest() {
+        RBucketReactive<String> bucket = this.client.getBucket("user:1:name", StringCodec.INSTANCE);
+        Mono<Void> set = bucket.set("Fahim Faysal", 1, TimeUnit.SECONDS);
+        Mono<String> get = bucket.get();
+
+        // Testing if the key is saved to redis
+        StepVerifier
+                .create(set.then(get))
+                .expectNextCount(1)
+                .verifyComplete();
+
+        // Extend the expiry time
+        Mono<Boolean> extend = bucket.expire(Duration.ofSeconds(3));
+
+        // Testing if the expiry time has successfully extended
+        StepVerifier
+                .create(extend)
+                .expectNext(true)
+                .verifyComplete();
+
+        // Testing if the key exist in redis after 2 seconds
+        sleep(2000);
+
+        StepVerifier
+                .create(get)
+                .expectNextCount(1)
                 .verifyComplete();
     }
 
