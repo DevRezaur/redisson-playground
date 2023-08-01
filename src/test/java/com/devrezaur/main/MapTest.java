de9928766dddcac34c53697f0f0d65262aea4951
Rezaur Rahman <os-rezaur.rahman@rakuten.com>
2023-08-01 08:51:09 +0600

    Updated MapTest

--- src/test/java/com/devrezaur/main/MapTest.java
+++ src/test/java/com/devrezaur/main/MapTest.java
@@ -1,12 +1,15 @@
 package com.devrezaur.main;
 
+import com.devrezaur.main.model.Student;
 import org.junit.jupiter.api.Test;
 import org.redisson.api.RMapReactive;
 import org.redisson.client.codec.StringCodec;
+import org.redisson.codec.TypedJsonJacksonCodec;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -56,10 +59,46 @@ public class MapTest extends BaseTestClass {
                 .verifyComplete();
     }
 
+    @Test
+    public void mapTest3() {
+        // Set some map data in redis
+        TypedJsonJacksonCodec codec = new TypedJsonJacksonCodec(Integer.class, Student.class);
+        RMapReactive<Integer, Student> map = this.client.getMap("students", codec);
+
+        Student student1 = new Student("Rezaur Rahman", 25, "Dhaka", List.of("Physics", "Biology"));
+        Student student2 = new Student("Fahim Faysal", 35, "Rangpur", List.of("Math", "History"));
+
+        Mono<Student> mono1 = map.put(1, student1);
+        Mono<Student> mono2 = map.put(2, student2);
+
+        // Test if the set method is executed successfully
+        StepVerifier
+                .create(mono1.concatWith(mono2))
+                .expectNext(student1)
+                .expectNext(student2)
+                .verifyComplete();
+
+        // Get map data from redis
+        Mono<Map<Integer, Student>> get = map.getAll(Set.of(1, 2));
+
+        // Test the retrieved values from redis
+        StepVerifier
+                .create(get)
+                .expectNext(getStudentMap())
+                .verifyComplete();
+    }
+
     private HashMap<String, String> getUserMap() {
         HashMap<String, String> user = new HashMap<>();
         user.put("name", "Rezaur Rahman");
         user.put("city", "Dhaka");
         return user;
+    }
+
+    private HashMap<Integer, Student> getStudentMap() {
+        HashMap<Integer, Student> studentMap = new HashMap<>();
+        studentMap.put(1, new Student("Rezaur Rahman", 25, "Dhaka", List.of("Physics", "Biology")));
+        studentMap.put(2, new Student("Fahim Faysal", 35, "Rangpur", List.of("Math", "History")));
+        return studentMap;
     }
 }
