d2646b5ce0092ef909c9d85883307661b3f71f27
Rezaur Rahman <os-rezaur.rahman@rakuten.com>
2023-08-02 09:45:45 +0600

    Updated ListQueueStackTest

--- src/test/java/com/devrezaur/main/ListQueueStackTest.java
+++ src/test/java/com/devrezaur/main/ListQueueStackTest.java
@@ -1,7 +1,9 @@
 package com.devrezaur.main;
 
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RDequeReactive;
 import org.redisson.api.RListReactive;
+import org.redisson.api.RQueueReactive;
 import org.redisson.client.codec.LongCodec;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -60,6 +62,92 @@ public class ListQueueStackTest extends BaseTestClass {
         StepVerifier
                 .create(list.size())
                 .expectNext(5)
+                .verifyComplete();
+    }
+
+    @Test
+    public void queueTest() {
+        RQueueReactive<Long> queue = this.client.getQueue("number-input", LongCodec.INSTANCE);
+
+        // Clearing the queue
+        queue.delete().block();
+
+        // Set some queue data in redis
+        List<Long> longList = LongStream.range(1, 6).boxed().toList();
+        Mono<Boolean> mono = queue.addAll(longList);
+
+        // Testing if the operation is successful
+        StepVerifier
+                .create(mono)
+                .expectNext(true)
+                .verifyComplete();
+
+        // Verifying the queue size
+        StepVerifier
+                .create(queue.size())
+                .expectNext(5)
+                .verifyComplete();
+
+        // Polling data from the queue
+        Flux<Long> flux = queue.poll().repeat(4).doOnNext(System.out::println);
+
+        // Verifying the polled data
+        StepVerifier
+                .create(flux)
+                .expectNext(1L)
+                .expectNext(2L)
+                .expectNext(3L)
+                .expectNext(4L)
+                .expectNext(5L)
+                .verifyComplete();
+
+        // Verifying the queue size again
+        StepVerifier
+                .create(queue.size())
+                .expectNext(0)
+                .verifyComplete();
+    }
+
+    @Test
+    public void stackTest() {
+        RDequeReactive<Long> deque = this.client.getDeque("number-input", LongCodec.INSTANCE);
+
+        // Clearing the deque
+        deque.delete().block();
+
+        // Set some deque data in redis
+        List<Long> longList = LongStream.range(1, 6).boxed().toList();
+        Mono<Boolean> mono = deque.addAll(longList);
+
+        // Testing if the operation is successful
+        StepVerifier
+                .create(mono)
+                .expectNext(true)
+                .verifyComplete();
+
+        // Verifying the deque size
+        StepVerifier
+                .create(deque.size())
+                .expectNext(5)
+                .verifyComplete();
+
+        // Polling data from the deque
+        Flux<Long> flux = deque.pollLast().repeat(4).doOnNext(System.out::println);
+
+        // Verifying the polled data
+        StepVerifier
+                .create(flux)
+                .expectNext(5L)
+                .expectNext(4L)
+                .expectNext(3L)
+                .expectNext(2L)
+                .expectNext(1L)
+                .verifyComplete();
+
+        // Verifying the deque size again
+        StepVerifier
+                .create(deque.size())
+                .expectNext(0)
                 .verifyComplete();
     }
 }
